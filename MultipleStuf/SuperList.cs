690cf0bc85fb78467359cb69e1c5a4fb91d35687
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-29 19:09:31 +0100

    Add Reference tuples ( SuperList)

--- MultipleStuf/SuperList.cs
+++ MultipleStuf/SuperList.cs
@@ -9,7 +9,9 @@ namespace MultipleStuf
 
 
         // Constructor classe herda uma lista de doubles
-        public SuperList() : base() {} 
+        public SuperList() : base() 
+        {
+        } 
 
         // 
         public void GetMinMax1(out double min, out double max)
@@ -32,18 +34,77 @@ namespace MultipleStuf
             }
         }
 
-        public void GetMinMax2()
+        public class GetMinMax2
         {
+            /*
+            double min = 0, max = 0;
+
+            public GetMinMax2(SuperList doubles)
+            {
+
+            }
+
+
+            foreach(double number in this)
+            {
+                if(number < min) // 5,  //min = 7
+                {
+                    min = number;
+                }
+
+                else if(number > max) // max = 7
+                {
+                    max = number;
+                }
+            }
+            */
 
         }
 
-        public void GetMinMax3()
+        public Tuple<double, double> GetMinMax3()
         {
+
+            
+            double min = this[0]; // 7
+            double max = this[0]; // 7
+            
+            foreach(double number in this)
+            {
+                if(number < min) // 5,  //min = 7
+                {
+                    min = number;
+                }
+
+                else if(number > max) // max = 7
+                {
+                    max = number;
+                }
+            }
+
+            // returning ref type values
+            return new Tuple<double, double> (min, max);
+
 
         }
 
-        public void GetMinMax4()
+        public  GetMinMax4()
         {
+
+            double min = this[0]; // 7
+            double max = this[0]; // 7
+            
+            foreach(double number in this)
+            {
+                if(number < min) // 5,  //min = 7
+                {
+                    min = number;
+                }
+
+                else if(number > max) // max = 7
+                {
+                    max = number;
+                }
+            }
 
         }
 
